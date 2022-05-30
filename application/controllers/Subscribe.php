6126b24f6a050d7f4312d5a29673daccaf048f92
Harki Ramadhan <harkiramadhan@Harkis-MacBook-Pro.local>
2022-05-30 18:16:17 +0700

    updates

--- application/controllers/Subscribe.php
+++ application/controllers/Subscribe.php
@@ -2,12 +2,17 @@
 class Subscribe extends CI_Controller{
     function add(){
         if($this->input->post('email', TRUE) != NULL || $this->input->post('email', TRUE) != ''){
-            $datas = [
-                'email' => $this->input->post('email', TRUE),
-                'status' => 1
-            ];
-    
-            $this->db->insert('subscribe', $datas);
+            $cek = $this->db->get_where('subscribe', ['email' => $this->input->post('email', TRUE)]);
+            if($cek->num_rows() > 0){
+                redirect($_SERVER['HTTP_REFERER'],'refresh');
+            }else{
+                $datas = [
+                    'email' => $this->input->post('email', TRUE),
+                    'status' => 1
+                ];
+        
+                $this->db->insert('subscribe', $datas);
+            }
         } 
         redirect($_SERVER['HTTP_REFERER'],'refresh');
     }
